2cf3574a1681e1331181130f6cf8b547c1454391
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-09-11 15:55:49 +0300

    feat: do task Tower of Hanoi

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  const turns = calculateTurns(disksNumber);
+  const seconds = calculateSeconds(turns, turnsSpeed);
+  return {turns, seconds};
 };
+
+function calculateTurns(disksNumber) {
+  return Math.pow(2, disksNumber) - 1;
+}
+function calculateSeconds(turns, turnsSpeed) {
+  return Math.floor((3600 / turnsSpeed) * turns);
+} 
