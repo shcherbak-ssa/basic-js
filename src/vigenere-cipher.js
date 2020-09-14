913e9f99fa3a74578f72bb8c6dd0fdcc461c191f
Stanislav Shcherbakov <shcherbak.ssa@gmail.com>
2020-09-14 20:37:57 +0300

    feat: do task Vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -7,25 +7,79 @@ class VigenereCipheringMachine {
 
   /** public mehtods */
   encrypt(message, key) {
-    throw new CustomError('Not implemented');
     if (this._isAdsenceParams(message, key)) throw new Error();
     
-    return this._transformResult(result);
+    const encryptMessage = this._doCrypt(message, key, 'encrypt');;
+    return this._transformResult(encryptMessage);
   }    
   decrypt(encryptedMessage, key) {
-    throw new CustomError('Not implemented');
     if (this._isAdsenceParams(encryptedMessage, key)) throw new Error();
-    
-    return this._transformResult(result);
+
+    const decryptMessage = this._doCrypt(encryptedMessage, key, 'decrypt');
+    return this._transformResult(decryptMessage);
   }
 
   /** private methods */
   _isAdsenceParams(message, key) {
     return message === undefined || key === undefined;
   }
-  _isValidChar(char) {
-    return (char >= 65 && char <= 90) || (char >= 97 && char <= 122);
+
+  _doCrypt(message, key, action) {
+    const actionFormula = this._getActionFormule(action);
+    const [messageCodes, keywordCodes] = this._transformParams(message, key);
+    const messageLen = message.length;
+    const cryptMessage = [];
+
+    let j = 0, i = 0, code = 0;
+    for (; i < messageLen; i += 1) {
+      const messageCodeItem = messageCodes[i];
+      if (this._isLetterCode(messageCodeItem)) {
+        code = (actionFormula(messageCodeItem, keywordCodes[i - j]) % 26) + 65;
+      } else {
+        code = messageCodeItem;
+        j += 1;
+      }
+      
+      const char = String.fromCharCode(code);
+      cryptMessage.push(char);
+    }
+
+    return cryptMessage;
   }
+  _getActionFormule(action) {
+    switch (action) {
+      case 'encrypt': return (messageCodeItem, keywordCodeItem) => {
+        return messageCodeItem + keywordCodeItem
+      };
+      case 'decrypt': return (messageCodeItem, keywordCodeItem) => {
+        return messageCodeItem - keywordCodeItem + 26
+      };
+    }
+  }
+  _isLetterCode(code) {
+    return code >= 65 && code <= 90
+  }
+
+  _transformParams(message, key) {
+    const keyword = this._transformKeyword(message, key);
+    const messageCodes = this._transformCharToCode(message);
+    const keywordCodes = this._transformCharToCode(keyword);
+    return [messageCodes, keywordCodes];
+  }
+  _transformKeyword(message, key) {
+    const keyLen = key.length; 
+    const keyword = [];
+    for (let i = 0; ; i += 1) {
+      if (keyLen === i) i = 0;
+      if (keyword.length === message.length) break; 
+      keyword.push(key[i]);
+    } 
+    return keyword.join('');
+  }
+  _transformCharToCode(str) {
+    return str.toUpperCase().split('').map((item) => item.charCodeAt(0));
+  }
+
   _transformResult(result) {
     if (!this._direct) result = result.reverse();
     return result.join('').toUpperCase()
